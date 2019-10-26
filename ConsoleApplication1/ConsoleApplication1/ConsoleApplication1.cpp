b8aea73616275191a922ad5b063d358e511fdeaa
yjduoduo <wxjlmr@126.com>
2019-10-26 21:11:00 +0800

    一个简单的控制台工程

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+//#include<iostream.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i = 0;
+	for (i = 0; i < 10;i++)
+	{
+		printf("hello the world!!\n");
+	}
+	
+	//_wsystem("pause");
+	//system("pause");
+
+	return 0;
+}
+
