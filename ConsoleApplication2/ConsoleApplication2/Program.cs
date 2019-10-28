56c50ee1c19f8721f06743899c0c92a89efefe6d
weiwei <wxjlmr@126.com>
2019-10-28 08:39:47 +0800

    Write and WriteLine

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -14,10 +14,26 @@ namespace ConsoleApplication2
             Console.WriteLine(nm.ToUpper());
             Console.ReadKey();
         }
+        static void OneShiGe()
+        {
+            Console.Write("白日依山尽");
+            Console.Write("黄河入海流");
+            Console.Write("欲穷千里目");
+            Console.Write("更上一层楼");
+            Console.WriteLine("\n");
+            Console.WriteLine("锄禾日当午");
+            Console.WriteLine("汗滴禾下土");
+            Console.WriteLine("谁知盘中餐");
+            Console.WriteLine("粒粒皆辛苦");
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Hello C#");
-            Console.ReadKey();
+            //Console.Write("Hello C#");
+            //Console.ReadKey();
+            OneShiGe();
+
 
             test();
         }
