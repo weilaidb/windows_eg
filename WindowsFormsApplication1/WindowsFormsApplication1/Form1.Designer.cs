3d77159ffeabcd1e7ada98381b6cb713c42a81ce
weiwei <wxjlmr@126.com>
2019-10-28 20:56:26 +0800

    some example of base syntax

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,11 +29,22 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(83, 63);
+            this.button1.Location = new System.Drawing.Point(32, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 70);
             this.button1.TabIndex = 0;
@@ -41,21 +52,138 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(163, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(257, 65);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(78, 16);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(32, 101);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 84);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(163, 164);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(134, 163);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(86, 245);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(65, 12);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(163, 246);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(65, 12);
+            this.linkLabel2.TabIndex = 8;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "linkLabel2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(257, 190);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 9;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(34, 305);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 21);
+            this.maskedTextBox1.TabIndex = 10;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(409, 27);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(729, 405);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -15,11 +15,148 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+            loadtext();
+            eg1();
+            eg2();
+            eg3();
+            eg4();
+            eg5_replace();
+            eg6_parse();
+            eg7_convert();
+            eg8_sum();
+            eg9_multiply();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Console.WriteLine("the first button");
+            this.Text = System.DateTime.Now.ToString();
+            MessageBox.Show(@"Hello\Form");
+        }
 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, Form");
+        }
+        private void loadtext()
+        {
+            string result = "";
+            result += "山东省\t济南\r";
+            result += "山东省\t济南\r";
+            result += "山东省\t济南\r";
+            result += "山东省\t济南\r";
+            result += "山东省\t济南\r";
+            result += "山东省\t济南\r";
+            this.maskedTextBox1.Text = result;
+
+        }
+        private void eg1()
+        {
+            string s1 = "micro";
+            string s2 = s1 + "off";
+
+            Console.WriteLine(s2);
+
+            string s3 = (2.232 + 3.4434).ToString();
+            string s4 = "" + 2.7832 + 323;
+            Console.WriteLine(s4);
+        }
+
+        private void eg2()
+        {
+            string s1 = "micro, test for it ";
+            string s2 = s1.Substring(3, 4);
+            //Substring提取字符s1，从第三个字符起连续提取4个。
+        }
+
+        private void eg3()
+        {
+            string s1 = "officew wwwww ";
+            int p = s1.IndexOf("go");
+            Console.WriteLine("index is " + p);
+        }
+
+        private void eg4()
+        {
+            string s0 = "format", s1 = "s1", s2 = "s2", s3 = "s3", s4 = "s4", s5 = "s5";
+            string result = "";
+            result = String.Format("result is {0} {3} {4}", s0, s1, s2, s3, s4, s5);
+            Console.WriteLine("result is " + result);
+
+        }
+
+        private void eg5_replace()
+        {
+            string s = "microsoft office";
+            string t = s.Replace('o', '%');
+            string u = s.Replace("oso", "db");
+            Console.WriteLine("s is " + s);
+            Console.WriteLine("t is " + t);
+            Console.WriteLine("u is " + u);
+        }
+
+        private void eg6_parse()
+        {
+            string temp = "1";
+            int i = int.Parse(temp);
+            float f = float.Parse(temp);
+            char c = char.Parse(temp);
+
+            string s = "false";
+            bool b = bool.Parse(s);
+
+            string d = "2016-8-8";
+            DateTime dt = DateTime.Parse(d);
+
+
+            Console.WriteLine(i);
+            Console.WriteLine(f);
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+            Console.WriteLine(dt);
+
+        }
+
+        private void eg7_convert()
+        {
+            double p = System.Convert.ToDouble("3.14");
+            Console.WriteLine(p);
+        }
+
+        private void eg8_sum()
+        {
+            int[] n = new int[10];
+            for (int i =  0; i < n.Length; i++)
+            {
+                n[i] = i + 1;
+            }
+
+            int total = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                total += n[i];
+            }
+            //MessageBox.Show(total.ToString());
+            Console.WriteLine("total is " + total.ToString());
+        }
+
+        private void eg9_multiply()
+        {
+            string[,] t = new string[9, 9];
+            string result = "";
+            int R, C;
+            for(int r = 0; r < 9; r++)
+            {
+                result += "\n";
+                R = r + 1;
+                for(int c = 0; c <= r; c++)
+                {
+                    C = c + 1;
+                    result += C + "*" + R + "=" + (R * C) + " ";
+                }
+            }
+            MessageBox.Show(result);
         }
     }
 }
