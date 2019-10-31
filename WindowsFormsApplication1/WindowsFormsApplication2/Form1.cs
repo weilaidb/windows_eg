9db652a150ef3943c64fa58d7a131c3acdb44a40
weiwei <wxjlmr@126.com>
2019-10-31 21:34:37 +0800

    添加打开Excel的对象信息

--- WindowsFormsApplication1/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication2/Form1.cs
@@ -41,5 +41,60 @@ namespace WindowsFormsApplication2
             }
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Excel.Application newApp = new Excel.Application();
+            newApp.Visible = true;
+            newApp.Caption = "new app";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //获取正在运行的Excel
+            try
+            {
+                ExcelApp = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+                //MessageBox.Show("Excel的版本是：" + ExcelApp.Version + "");
+
+                ((Excel.Worksheet)ExcelApp.ActiveSheet).Name = "MysSheet";//重命名活动工作表
+
+                Excel.Range rng = (Excel.Range)ExcelApp.Selection;//Excel对象赋值给对象变量rng
+                ExcelApp.StatusBar = "My Excel Application"; //改变Excel状态栏文字
+                MessageBox.Show(ExcelApp.Workbooks.Count + "");//显示工作簿个数
+                MessageBox.Show(ExcelApp.UserName + "");//显示用户名
+                ExcelApp.DisplayFormulaBar = false;//隐藏公式编辑栏
+
+
+            }
+            catch(SystemException ex)
+            {
+                //MessageBox.Show(ex.Message);
+                MessageBox.Show("请打开Excel文件");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            //获取正在运行的Excel
+            try
+            {
+                ExcelApp = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+                //MessageBox.Show("Excel的版本是：" + ExcelApp.Version + "");
+                ExcelApp.Undo();//Excel撤销，相当于按下Ctrl+Z
+                ExcelApp.Workbooks.Close();
+                ExcelApp.Quit();
+
+            }
+            catch (SystemException ex)
+            {
+                //MessageBox.Show(ex.Message);
+                MessageBox.Show("请打开Excel文件");
+            }
+
+        }
+
+
     }
 }
